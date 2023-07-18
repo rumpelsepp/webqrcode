9b99ee4a29842fb9de9cac389d73b58cc09b80b7
Stefan Tatschner <stefan@rumpelsepp.org>
2023-07-18 11:36:47 +0200

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    with import nixpkgs { system = "x86_64-linux"; };
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go gopls gotools
+        ];
+      };
+      defaultPackage.x86_64-linux =
+      pkgs.buildGoModule {
+        pname = "webqrcode";
+        src = self;
+        version = self.lastModifiedDate;
+        buildInputs = [pkgs.go];
+        vendorHash = "sha256-rUQ/iXG/lnvi01NXBOsu3CoG3YrYUFwPZDadTFThH3g=";
+      };
+      formatter.x86_64-linux = pkgs.nixpkgs-fmt;
+    };
+}
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/yeqown/go-qrcode/v2"
+	"github.com/yeqown/go-qrcode/writer/standard"
+)
+
+type nopCloser struct {
+	io.ReadWriter
+}
+
+func (*nopCloser) Close() error {
+	return nil
+}
+
+func createQR(data string) ([]byte, error) {
+	qrc, err := qrcode.New(data)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf bytes.Buffer
+	options := standard.WithBuiltinImageEncoder(standard.PNG_FORMAT)
+	w := standard.NewWithWriter(&nopCloser{&buf}, options)
+
+	if err := qrc.Save(w); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+type qrserver struct{}
+
+func (s *qrserver) handleGET(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, `
+<!DOCTYPE html>
+<head>
+<title>Steff's QR Code Generator</title>
+<meta charset="utf-8">
+</head>
+<body>
+ <form action="/qr" method="post">
+  <label for="data">Data</label><br>
+  <input type="text" id="data" name="data"><br>
+  <input type="submit" value="Submit">
+</form> 
+</body>
+`)
+}
+
+func (s *qrserver) handlePOST(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	data := r.FormValue("data")
+	qrData, err := createQR(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+	w.Write(qrData)
+}
+
+func (s *qrserver) serve(listen string) error {
+	r := mux.NewRouter()
+	r.HandleFunc("/", s.handleGET)
+	r.HandleFunc("/qr", s.handlePOST)
+	srv := &http.Server{
+		Addr:         listen,
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r,
+	}
+	if err := srv.ListenAndServe(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return nil
+}
+
+func main() {
+	s := qrserver{}
+	if err := s.serve("localhost:8888"); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
